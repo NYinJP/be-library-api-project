386f64a7a9fde194040939bb9fbea8849fd07973
sksDud <hinagogo@naver.com>
2023-08-04 17:36:30 +0900

    package

---
+++ src/main/Java/com/example/member/MemberController.java
@@ -0,0 +1,58 @@
+package com.example.member;
+
+import com.example.member.dto.MemberPatchDto;
+import com.example.member.dto.MemberPostDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/members")
+public class MemberController {
+
+    //회원 정보 등록
+    @PostMapping
+    public ResponseEntity postMember(@RequestBody MemberPostDto memberPostDto) {
+        return new ResponseEntity<>(memberPostDto, HttpStatus.OK);
+    }
+    // 회원 정보 수정
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(@PathVariable("member-id") long memberId,
+                                      @RequestBody MemberPatchDto memberPatchDto) {
+        memberPatchDto.setMemberId(memberId);
+        memberPatchDto.setName("홍길동");
+
+        // No need Business logic
+
+        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
+    }
+
+    // 한명의 회원 정보 조회
+    @GetMapping("/{member-id}")
+    public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
+        System.out.println("# memberId: " + memberId);
+
+        // not implementation
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 모든 회원 정보 조회
+    @GetMapping
+    public ResponseEntity getMembers() {
+        System.out.println("# get Members");
+
+        // not implementation
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 회원 정보 삭제
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
+        // No need business logic
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
+
+
